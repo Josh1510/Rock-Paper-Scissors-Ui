f7674851cd955217127e63aa9e8d80b6e33ad6b5
josh1510 <joshmoon.1510@gmail.com>
2021-02-22 17:59:21 +1100

    added event listener to images, when image clicked playRound() is called

--- javascript/game-logic.js
+++ javascript/game-logic.js
@@ -1,6 +1,21 @@
-let playableOptions = ['Rock', 'Paper', 'Scissors'];
+let playableOptions = ['rock', 'paper', 'scissors'];
+
 let userScore = 0;
 let computerScore = 0;
+let userSelection = '';
+
+const userOptions = document.querySelectorAll('.userOption');
+
+userOptions.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        userSelection = event.target.id;
+
+        userSelection = playableOptions.indexOf(userSelection);
+        console.log(userSelection);
+
+        playRound(computerSelection(), userSelection);
+    });
+});
 
 // computerPlay function will randomly select then return "Rock", "Paper", or "Scissors"
 function computerSelection() {
@@ -12,20 +27,21 @@ function computerSelection() {
 // entered string is converted to lower case then first letter capitalised
 // then returns index of the option from the playableOptions array
 
-function userSelection() {
-    let userInput = prompt('Please enter your selection. Rock, Paper, or Scissors!');
-    userInput = userInput.toLowerCase();
-    userInput = userInput.charAt(0).toUpperCase() + userInput.slice(1);
+// function userSelection() {
+//     let userInput = prompt('Please enter your selection. Rock, Paper, or Scissors!');
+//     userInput = userInput.toLowerCase();
+//     userInput = userInput.charAt(0).toUpperCase() + userInput.slice(1);
 
-    if (playableOptions.indexOf(userInput) == -1) {
-        console.log(userInput + ' is not a valid option, please try again.');
-        userSelection();
-    } else {
-        return playableOptions.indexOf(userInput);
-    }
-}
+//     if (playableOptions.indexOf(userInput) == -1) {
+//         console.log(userInput + ' is not a valid option, please try again.');
+//         userSelection();
+//     } else {
+//         return playableOptions.indexOf(userInput);
+//     }
+// }
 
 //playRound function plays a round then increases the score of the winner
+//try {
 function playRound(computerSelection, userSelection) {
     if (computerSelection === userSelection) {
         return console.log(
@@ -55,10 +71,12 @@ function playRound(computerSelection, userSelection) {
         );
     }
 }
-
+//} catch (err) {
+//    console.log('game exited, want to try again? Please refresh the page');
+//}
 //This function loops 5 times to play a best of 5, prints the score at the end of each round then
 //declares a winner
-function game() {
+function playGame() {
     console.log('Best of 5!');
 
     const gameLength = 5;
@@ -75,8 +93,8 @@ function game() {
         console.log('User wins with ' + userScore + ' points! Computer only had ' + computerScore);
     }
 }
-try {
-    game();
-} catch (err) {
-    console.log('game exited, want to try again? Please refresh the page');
-}
+// try {
+//     playGame();
+// } catch (err) {
+//     console.log('game exited, want to try again? Please refresh the page');
+// }
