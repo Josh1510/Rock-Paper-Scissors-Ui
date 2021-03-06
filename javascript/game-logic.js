e77b72c10cfe64d52ed00fc5cffb9847030f6e0b
Joshua Moon <joshmoon.1510@gmail.com>
2021-03-06 16:32:07 +1100

    .

--- javascript/game-logic.js
+++ javascript/game-logic.js
@@ -5,6 +5,8 @@ let computerScore = 0;
 let userSelection = '';
 let roundsPlayed = 0;
 
+let newPara = document.createElement('p');
+
 const userOptions = document.querySelectorAll('.user-option');
 const scoreUpdate = document.querySelectorAll('.score');
 const resultsUpdate = document.querySelectorAll('.result');
@@ -29,60 +31,45 @@ function computerSelection() {
     return playableOptions.indexOf(computerChoice);
 }
 
+function printResults(results) {
+    var para = document.createElement('p');
+    var node = document.createTextNode(results);
+    para.append(node);
+    resultsUpdate[0] = resultsUpdate[0].appendChild(para);
+}
+
 //playRound function plays a round then increases the score of the winner
 function playRound(computerSelection, userSelection) {
-    roundsPlayed++;
-
     if (computerSelection === userSelection) {
         resultsUpdate[0].textContent = 'Results: draw';
 
+        printResults('draw');
         // var para = document.createElement('p');
         // var node = document.createTextNode('was a draw.');
-        // para.appendChild(node);
-        // resultsUpdate[0].appendChild(para);
-
-        return console.log(
-            'computer selected: ' +
-                playableOptions[computerSelection] +
-                '. User selected: ' +
-                playableOptions[userSelection] +
-                ". It's a DRAW! No points"
-        );
+        // para.append(node);
+        // resultsUpdate[0].append(para);
     } else if ((computerSelection + 1) % 3 == userSelection) {
         userScore++;
         scoreUpdate[1].textContent = 'User Score: ' + userScore;
         resultsUpdate[0].textContent = 'Results: userwin';
 
+        printResults('user wins');
         // var para = document.createElement('p');
         // var node = document.createTextNode('user wins');
-        // para.appendChild(node);
-        // resultsUpdate[0].appendChild(para);
-
-        return console.log(
-            'computer selected: ' +
-                playableOptions[computerSelection] +
-                '. User selected: ' +
-                playableOptions[userSelection] +
-                '. User Wins! User gets 1 point'
-        );
+        // para.append(node);
+        // resultsUpdate[0].append(para);
     } else {
         computerScore++;
         scoreUpdate[0].textContent = 'Computer Score: ' + computerScore;
         resultsUpdate[0].textContent = 'Results: computerwin';
 
+        printResults('computer wins');
         // var para = document.createElement('p');
         // var node = document.createTextNode('computer wins.');
-        // para.appendChild(node);
-        // resultsUpdate[0].appendChild(para);
-
-        return console.log(
-            'computer selected: ' +
-                playableOptions[computerSelection] +
-                '. User selected: ' +
-                playableOptions[userSelection] +
-                '. Computer Wins! Computer gets 1 point'
-        );
+        // para.append(node);
+        // resultsUpdate[0].append(para);
     }
+    roundsPlayed++;
 }
 
 //This function loops 5 times to play a best of 5, prints the score at the end of each round then
